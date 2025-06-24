073b0061ee69ab50460bb838b5cfaf35c8d32be7
mino-hobby-pro <ino.haru.426@gmail.com>
2025-06-24 21:51:40 +0900

    Update uv.config.js

--- uv/uv.config.js
+++ uv/uv.config.js
@@ -3,7 +3,7 @@ self.__uv$config = {
     prefix: '/service/',
 
     /* Bare server URL */ 
-    bare: 'https://bare.electrial.site',
+    bare: 'https://bare-server.akku1139.workers.dev/',
     
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
